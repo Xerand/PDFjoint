e54800017d302334c6e83cc5bca4a4983ed7a5b1
Andrea Monti <andreamonti@live.it>
2024-06-16 19:02:17 +0200

    PDFjoint

---
+++ PDFjoint.py
@@ -0,0 +1,63 @@
+from os import *
+from PyPDF2 import PdfMerger
+
+
+def Banner():
+    banner = """
+  ____  ____  _____ _       _       _    
+ |  _ \|  _ \|  ___(_) ___ (_)_ __ | |_  
+ | |_) | | | | |_  | |/ _ \| | '_ \| __| 
+ |  __/| |_| |  _| | | (_) | | | | | |_  
+ |_|   |____/|_|  _/ |\___/|_|_| |_|\__| 
+                 |__/                    
+    """
+    sottotitolo = "© A.Monti 2024\n"
+    utilizzo = """Usage:
+1) Rename the files that are to be merged like this: 00.pdf, 01.pdf, 02.pdf... etc.
+2) Place them in the folder that contains PDFjoint.exe
+3) Start PDFjoint.exe
+CTRL+c to exit"""
+    print(banner)
+    print(sottotitolo)
+    print(utilizzo)
+
+def Controllo_nome(nome):
+    nome_iniz = nome[0:-4]
+    if nome.endswith(".pdf") and nome_iniz.isnumeric():
+        return True
+    else:
+        return False
+
+Banner()
+
+x = [a for a in listdir() if Controllo_nome(a)]
+
+if not x:
+    print("\nThere are no files to join")
+    input("Press a key to exit...")
+    exit()
+
+nome_file = input("\nEnter the name of the final file >>> ")
+
+if nome_file == "":
+    nome_file = "_jointPDF.pdf" 
+else:
+    nome_file = nome_file + ".pdf"
+
+print("\nFiles to join")
+for i in range(0, len(x)):
+    print(f"File {i+1}: {x[i]}")
+
+while True:
+    scelta = input("\nPress return to merge or 0 to exit >>> ")
+
+    if scelta == "0":
+        exit()
+    if scelta == "":
+        merger = PdfMerger()
+        for pdf in x:
+            merger.append(pdf)
+        merger.write(nome_file)
+        merger.close()
+        exit()
+
